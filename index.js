8e6fa7c149c658332c6ca36153ff32232a9e44a3
MuradsGIT <151630356+MuradsGIT@users.noreply.github.com>
2023-12-02 14:06:20 +0400

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
+     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization']
   })
 );
 app.use(express.json());

066322fa9c395cc951b0f270d1bb9047304acc49
MuradsGIT <151630356+MuradsGIT@users.noreply.github.com>
2023-12-02 14:37:22 +0400

    u index.js

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ const createToken = (_id) => {
 };
 
 // important stuff
-const corsOptions ={
-    origin:'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 

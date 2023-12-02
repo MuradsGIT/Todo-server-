00807338375b9e48522b9d3880cd075ea9e9e783
MuradsGIT <151630356+MuradsGIT@users.noreply.github.com>
2023-12-02 14:27:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -16,16 +16,14 @@ const createToken = (_id) => {
 };
 
 // important stuff
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    
-  })
-);
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.options('*', cors());
 
 // connection with mongodb
 mongoose.connect(process.env.MONGO);
